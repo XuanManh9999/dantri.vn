e0df49da90fadcb5a6d54e192a95b094e048d357
XuanManh9999 <nguyenxuanmanh030929@gmail.com>
2024-05-04 12:47:51 +0700

    Update

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,16 @@
 const button = document.querySelector(".fa-user");
 const containerBoxmodal = document.querySelector(".container-boxmodal");
+const boxmodal = document.querySelector(".boxmodal");
 const btnClose = document.querySelector(".container-boxmodal-close");
+
 button.onclick = () => {
   containerBoxmodal.style.display = "block";
 };
 btnClose.addEventListener("click", () => {
   containerBoxmodal.style.display = "none";
 });
+containerBoxmodal.onclick = (e) => {
+  if (e.target === containerBoxmodal) {
+    containerBoxmodal.style.display = "none";
+  }
+};
